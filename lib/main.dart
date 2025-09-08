4657d36bec144ce83f64a693ceb67f67ab3d038b
rashidjatoi <rashidjatoi31@gmail.com>
2025-09-08 16:17:10 +0200

    fixe

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'app.dart';
-import 'core/bindings/screen_bindings.dart';
-import 'core/utils/colors/app_colors.dart';
-import 'core/utils/app_routes.dart';
+
 
 void main() {
   runApp(const MyApp());
